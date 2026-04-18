49de4b720270c7019e72b96a784c1155dc3c6866
Vladislav <vladostap2003@gmail.com>
2026-04-18 19:19:01 +0300

    Adding a file extension when importing a module

--- ClothingStore/api/get-data.js
+++ ClothingStore/api/get-data.js
@@ -1,4 +1,4 @@
-import { menClothing, womenClothing } from "../server/data/cardData";
+import { menClothing, womenClothing } from "../server/data/cardData.js";
 
 export default function handler(req, res) {
   res.status(200).json({ menClothing, womenClothing });
